02804c47e9aa1e005af882723d205ed1e355e56d
Lisa Smith <las619@comcast.net>
2023-02-14 17:08:06 -0500

    Add labs for Weeks 1-3

---
+++ week01/Week01VariablesAndOperations.js
@@ -0,0 +1,39 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+
+		
+//create a variable to hold the cost of groceries at checkout
+
+
+//create a variable to hold a person's middle initial
+
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+
+//create a variable to hold a customer's first name
+
+
+//create a variable to hold a street address
+
+
+//print all variables to the console
+
+
+
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+
+
+//birth certificate was printed incorrectly, change the middle initial to something else
+
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+
+
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+
+
+//print a line to the console that introduces the customer and says they live at the address variable
