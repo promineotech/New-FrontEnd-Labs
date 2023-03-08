96c55e4199cb20b913308d4678bf78b37cc5e2df
ConwayCJ <conwaycj@plu.edu>
2023-03-08 09:25:36 -0800

    Add week06 lab

---
+++ week06/tests.js
@@ -0,0 +1,63 @@
+/* Copyright (c) 2023 Promineo Tech
+   Author:  Promineo Tech Academic Team
+   Subject:  JavaScript Mocha/Chai
+   JavaScript Lab Week 6 */
+const expect = chai.expect
+const assert = chai.assert
+
+/* ----------------------------------------------------- */
+// Resources:
+// expect Documentation: https://www.chaijs.com/api/bdd/
+// assert Documntation: https://www.chaijs.com/api/assert/
+//
+// assert is very similar to how you use expect, but offers
+// extra ways to test.
+
+/* ----------------------------------------------------- */
+// Please do not alter the existing code unless instructed to do so.
+// Read the comments and add your code where it is specified for each question.
+/* ----------------------------------------------------- */
+
+/**
+ *          YOU MUST 'npm install' IN YOUR TERMINAL TO INSTALL MOCHA/CHAI 
+ *          FROM THE DEPENDENCIES IN YOUR PACKAGE.JSON
+ * 
+ *  Step 1: Create a describe code block that describes what you expect the code to do.
+ *  Step 2: Copy/Paste your debugged code from week6Lab.js into the describe block (exclude the final console.log())
+ *  Step 3: Create tests using expect/assert to test for expected outputs. Use multiple cases.
+ *          If you're testing against an array/object - read the documentation on .deep/.deepEquals
+ *
+ *  Note:   Mocha/Chai is currently set up to only run in your given index.html. 
+ *          expect/assert are given to you at the top of the code. 
+ * 
+ *          By default, the tests will pass unless you give it code to test against.
+ * 
+/*--------------------------------------------------------------------*/
+
+describe('Week 6 Lab Tests:', () => {
+  describe('Example Question Test: Add Two Numbers', () => {
+    it('#Should return the sum of two numbers', () => {
+      // Copy & paste your debugged code from week6Lab.js
+      function addTwoNumbers(num1, num2) {
+        return num1 + num2
+      }
+      // Write tests to ensure it works for multiple examples
+      expect(addTwoNumbers(2, 3)).to.equal(5)
+      expect(addTwoNumbers(9, 17)).to.equal(26)
+      expect(addTwoNumbers(750, 250)).to.equal(1000)
+      expect(addTwoNumbers(132780, 443378)).to.equal(576158)
+    })
+
+   describe('Example Failed Test: Add Two Numbers', () => {
+    it('#Should fail', () => {
+      function sumOfTwoNumbers(num1, num2) {
+        num1 + num2
+      }
+
+      expect(sumOfTwoNumbers(2, 3)).to.equal(5)
+    })
+  })
+
+  })
+  /*--------------------------NEW TESTS BELOW-------------------------------*/
+})
