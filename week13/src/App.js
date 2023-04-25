df2de87384bf3b45d336339da19816a8f3d0bfb8
nereida-promineo <nereida.promineo@gmail.com>
2023-04-25 09:03:52 -0400

    Changed wording in Week 13 lab part 1 and typo

--- week13/src/App.js
+++ week13/src/App.js
@@ -31,14 +31,13 @@
  * Step 3: In your App.js, import your <Nav> component inside of the div in return()
  *         You should now see the text 'Nav' on your app.
  *         Don't forget to 'npm start' in the console if you aren't on a live server yet.
- *
- * Step 4: Instead of returning just a <div>, return a <nav> with an <ul>, and 3 <li>.
+ * Step 4: In your Nav.js file, add an <ul>, and 3 <li> inside your <nav> element.
  *         The text inside the 3 <li> should be: Home, Portfolio, and Contact
  * Step 5: Style your Nav component using the app.css file.
  *         Give your <nav> element: 100vw
  *                                  (100 view width makes the width the size of their screen)
  *         Give your <ul> element: display: flex, flex-direction: row, justify-content: right
- *         The rest is up to you! Consider peudo:selectors, such as hover/active/touch
+ *         The rest is up to you! Consider pseudo:selectors, such as hover/active/touch
  *
  *         Alternatively, refer to Week 9 and use bootstrap instead.
  *         Remember when we give elements classes in React, we use className=""
