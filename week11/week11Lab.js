2d040248ccb3054ef7774615979764b120013b15
nereida-promineo <nereida.promineo@gmail.com>
2023-05-03 09:24:50 -0400

    Added alternate way to start up db.json as requested by Matthew.

--- week11/week11Lab.js
+++ week11/week11Lab.js
@@ -102,13 +102,13 @@ $.get(CATS_API_URL, (data)=> {
 
 
 /* Instructions:
- * Step 1: Make an AJAX call to GET a random joke using this jokes API https://official-joke-api.appspot.com/random_joke.
- * Step 2: Using the URL, make an AJAX call to 'GET' a random joke. Print to data returned to the console.  
- * Step 3: Print the joke setup to the DOM by prepending it as a PARAGRAPH to the DIV with a class name of "jokes". (See documentation:https://api.jquery.com/prepend/)
- * Step 4: Print the joke punchline to the DOM by appending it as a PARAGRAPH to the DIV with a class name of "jokes". Save your changes. (See documentation: https://api.jquery.com/append/)
- * 
- * 
- * ↓ YOUR CODE HERE ↓ */
+ 	* Step 1: Make an AJAX call to GET a random joke using this jokes API https://official-joke-api.appspot.com/random_joke.
+ 	* Step 2: Using the URL, make an AJAX call to 'GET' a random joke. Print to data returned to the console.  
+ 	* Step 3: Print the joke setup to the DOM by prepending it as a PARAGRAPH to the DIV with a class name of "jokes". (See documentation:https://api.jquery.com/prepend/)
+ 	* Step 4: Print the joke punchline to the DOM by appending it as a PARAGRAPH to the DIV with a class name of "jokes". Save your changes. (See documentation: https://api.jquery.com/append/)
+ 	* 
+ 	* 
+ 	* ↓ YOUR CODE HERE ↓ */
 
 
 
@@ -123,21 +123,46 @@ $.get(CATS_API_URL, (data)=> {
 
 /* We are going to use a JSON Server as a fake API. APIs in general will be covered throughout the rest of this course. This is a way to practice making calls to an API without worrying about authentication and Keys which are usually required for using an API. In order to complete this lab you will have to do some extra installations. The db.json file for the json-server is already included with this lab.
 	* Step 1: In the terminal, change into this files directory and run the following commands:
-			`npm install -g json-server`
-			`json-server --watch db.json`
-	* Step 2: In the terminal you should see the link that you can use to make the calls, it should look something like this: 
-			Resources
-			http://localhost:3000/gradebook
 
-			Home
-			http://localhost:3000
+		METHOD #1
+			1. type command: npm install -g json-server
+			2. type command: json-server --watch db.json
+			3. In the terminal you should see the link that you can use to make the calls, it should look something like this:
 
-	* Step 3: Take a look at the db.json file and the data that is provided.The endpoint used to access this data will be 'gradebook' as shown in the Resources link. The Resources link will be your fake API url. 
-	* Step 4: Make a 'GET' request to the JSON server using the Resources link like an API url. Print the returned data to the console. Look at the console to compare the data printed and the data in the db.json file.  
-	* Step 5: Make another 'GET' request to the JSON server but after the ENDPOINT type in /7 this will GET the information for only the student with an ID of 7. Print the returned data to the console. 
-	* Step 6: In the GET calls anonymous function, write a JQuery method that will put the data in the empty DIV with a class name of "result". Access the student data with the object dot notation and concatenate the student data into a string that looks something like this: 
+				Resources
+				http://localhost:3000/gradebook
+
+				Home
+				http://localhost:3000
+
+			4. Click on the Resources link to open the json database on the browser, be sure to leave this terminal instance open so it can continue to watch the 'json' file
+			5. Open your HTML file on the browser using Live Server or whatever method you customarily use
+
+		(As some computers have issues with permissions, if 
+		  METHOD #1 didn't work for you then, alternatively, try METHOD #2.)
+		------------------------------------------------------
+		METHOD #2 
+	      Watches for the json-server db.json file:
+			1. type command: npm init -y
+			2. type command: npm i json-server
+			3. type command: npx json-server --watch db.json
+			4. This will output a url to your 'json' resource
+			5. Be sure to leave this terminal instance open
+
+		  Serves the 'website' url, (Alternate to Live Server):
+			1. Open up a second terminal instance
+			2. type command: npx serve
+			3. This will output a url to your 'website' with all of your files
+			4. Click on the HTML file to open it on the browser (this is the same as using Live Server but may avoid CORS issues)
+			5. You can also click on the db.json file to open it, just like the resources link
+
+
+	* Step 2: Take a look at the db.json file and the data that is provided.The endpoint used to access this data will be 'gradebook' as shown in the Resources link. The Resources link will be your fake API url. 
+	* Step 3: Make a 'GET' request to the JSON server using the Resources link like an API url. Print the returned data to the console. Look at the console to compare the data printed and the data in the db.json file.  
+	* Step 4: Make another 'GET' request to the JSON server but after the ENDPOINT type in /7 this will GET the information for only the student with an ID of 7. Print the returned data to the console. 
+	* Step 5: In the GET calls anonymous function, write a JQuery method that will put the data in the empty DIV with a class name of "result". Access the student data with the object dot notation and concatenate the student data into a string that looks something like this: 
 	*      (data.firstname + " " + data.lastname + ", Grade: "  + data.grade + "%")
-	* Step 7: Choose a different student Id and make another GET call that will print that  students data to the DOM like in Step 5 and 6, but this time use the DIV with a class name of "new".
+	* Step 6: Choose a different student Id and make another GET call that will print that  students data to the DOM like in Step 5 and 6, but this time use the DIV with a class name of "new".
 	* 
 	*
 	* ↓ YOUR CODE HERE ↓ */
